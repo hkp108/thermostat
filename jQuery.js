4d0bdcddd02f3f073db76f1d3afe4b7c0a97ea6c
adil ali <adiln1993@gmail.com>
2016-03-31 11:39:30 +0100

    implemented powersaving button

--- jQuery.js
+++ jQuery.js
@@ -14,3 +14,20 @@ $('#decreaseTemp').click(function(){
   thermostat.decreaseTemp();
   $('#display').html(thermostat.temp);
 });
+
+$('#resetTemp').click(function(){
+  thermostat.resetTemp();
+  $('#display').html(thermostat.temp);
+});
+
+$('#togglePowerSaving').click(function(){
+  thermostat.togglePowerSaving();
+  if(thermostat.powerSavingOn === true){
+    $('#power-saving-status').html('on');
+    thermostat.temp = 25;
+    $('#display').html(thermostat.temp);
+  }
+  if(thermostat.powerSavingOn === false) {
+    $('#power-saving-status').html('off');
+  }
+});
