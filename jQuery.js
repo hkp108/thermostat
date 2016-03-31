fe7da7143565da39efc4b945ac3fa8d4e96cdfb4
adil ali <adiln1993@gmail.com>
2016-03-31 11:13:48 +0100

    adding increase and decrease temp

--- jQuery.js
+++ jQuery.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-  alert("Welcome");
+document.onload = function() {
+  // thermostat = new Thermostat();
 };
+var thermostat = new Thermostat();
+$('#display').html(thermostat.temp);
+
+
+$('#increaseTemp').click(function(){
+  thermostat.increaseTemp();
+  $('#display').html(thermostat.temp);
+});
+
+$('#decreaseTemp').click(function(){
+  thermostat.decreaseTemp();
+  $('#display').html(thermostat.temp);
+});
